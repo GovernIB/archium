78c5332f7dbb73eaca3b11f2135c3ee5121b69b8
dsariego-memorandum <david.sariego@memorandum.net>
2021-02-16 16:20:53 +0100

    #16 Establecer la cardinalidad de los tipos documentales dentro de un procedimiento Se añade nuevo campo multiple en ACH_TIPUSDOCUMENT_PROCEDIMENT, con su correspondiente check en la vista

--- archium-back/src/main/java/es/caib/archium/objects/TipuDocumentalProcedimentObject.java
+++ archium-back/src/main/java/es/caib/archium/objects/TipuDocumentalProcedimentObject.java
@@ -13,6 +13,7 @@ public class TipuDocumentalProcedimentObject {
 	private TipuDocumentalObject	tipusDocumental;
 	private Boolean 				obligatori;
 	private Boolean 				recapitulatiu;
+	private Boolean					multiple;
 	
 	public TipuDocumentalProcedimentObject() {
 		super();
@@ -20,12 +21,13 @@ public class TipuDocumentalProcedimentObject {
 	}
 
 	public TipuDocumentalProcedimentObject(ProcedimentObject procediment, TipuDocumentalObject tipusDocumentalt, Boolean obligatori,
-			Boolean recapitulatiu) {
+			Boolean recapitulatiu, Boolean multiple) {
 		super();
 		this.procediment = procediment;
 		this.tipusDocumental = tipusDocumentalt;
 		this.obligatori = obligatori;
 		this.recapitulatiu = recapitulatiu;
+		this.multiple = multiple;
 	}
 	
 	public TipuDocumentalProcedimentObject(TipusdocumentProcediment i) {
@@ -42,6 +44,7 @@ public class TipuDocumentalProcedimentObject {
 		
 		this.obligatori = (i.getObligatori().intValue()==1 ? true : false);
 		this.recapitulatiu = (i.getRecapitulatiu().intValue()==1 ? true : false);
+		this.multiple = (i.getMultiple().intValue()==1 ? true : false);
 	}
 
 	public TipusdocumentProcediment toDbObject() {
@@ -58,6 +61,8 @@ public class TipuDocumentalProcedimentObject {
 		db.setId(pk);
 		db.setObligatori((obligatori==true ? new BigDecimal(1) : new BigDecimal(0)));
 		db.setRecapitulatiu((recapitulatiu==true ? new BigDecimal(1) : new BigDecimal(0)));
+		db.setMultiple((multiple==true ? new BigDecimal(1) : new BigDecimal(0)));
+
 		return db;
 	}
 	
@@ -100,46 +105,38 @@ public class TipuDocumentalProcedimentObject {
 	public void setRecapitulatiu(Boolean recapitulatiu) {
 		this.recapitulatiu = recapitulatiu;
 	}
-	
-	
-	
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
+
+	public Boolean getMultiple() {
+		return multiple;
+	}
+
+	public void setMultiple(Boolean multiple) {
+		this.multiple = multiple;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TipuDocumentalProcedimentObject other = (TipuDocumentalProcedimentObject) obj;
-		return Objects.equals(id, other.id);
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		TipuDocumentalProcedimentObject that = (TipuDocumentalProcedimentObject) o;
+		return Objects.equals(id, that.id) && Objects.equals(procediment, that.procediment) && Objects.equals(tipusDocumental, that.tipusDocumental) && Objects.equals(obligatori, that.obligatori) && Objects.equals(recapitulatiu, that.recapitulatiu) && Objects.equals(multiple, that.multiple);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, procediment, tipusDocumental, obligatori, recapitulatiu, multiple);
 	}
 
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("TipuDocumentalProcedimentObject [id=");
-		builder.append(id);
-		builder.append(", procediment=");
-		builder.append(procediment);
-		builder.append(", tipusDocumental=");
-		builder.append(tipusDocumental);
-		builder.append(", obligatori=");
-		builder.append(obligatori);
-		builder.append(", recapitulatiu=");
-		builder.append(recapitulatiu);
-		builder.append("]");
-		return builder.toString();
+		return "TipuDocumentalProcedimentObject{" +
+				"id=" + id +
+				", procediment=" + procediment +
+				", tipusDocumental=" + tipusDocumental +
+				", obligatori=" + obligatori +
+				", recapitulatiu=" + recapitulatiu +
+				", multiple=" + multiple +
+				'}';
 	}
-
-	
-
-	
-
 }

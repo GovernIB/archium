5d141e4688956fc7b40a661dacbf152b4c3837f4
dsariego-memorandum <david.sariego@memorandum.net>
2021-02-17 13:18:34 +0100

    #14 Revisar comportamiento campo serie esencial en pantalla Dictamen Se cambia serieesencial para que sea un combobox en el que se permita no seleccionar valor (valor nulo)

---
+++ archium-back/src/main/java/es/caib/archium/converters/BooleanConverter.java
@@ -0,0 +1,16 @@
+package es.caib.archium.converters;
+
+import es.caib.archium.controllers.ProcedimentController;
+import es.caib.archium.objects.AplicacioObject;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("BooleanConverter")
+public class BooleanConverter extends javax.faces.convert.BooleanConverter {
+
+}
--- archium-back/src/main/java/es/caib/archium/objects/DictamenObject.java
+++ archium-back/src/main/java/es/caib/archium/objects/DictamenObject.java
@@ -247,6 +247,8 @@ public class DictamenObject {
 		
 		if(serieEsencial!=null) {
 			db.setSerieessencial((serieEsencial ? new BigDecimal(1) : new BigDecimal(0)));
+		}else{
+			db.setSerieessencial(null);
 		}
 		
 		return db;
